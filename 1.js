3009515ff7a1e87eca688b27c1b4a955e4039b46
Deo Wibawa <deowibawa@gmail.com>
2019-09-14 15:33:37 +0700

    Jawaban Soal 1 dan keterangan

---
+++ 1.js
@@ -0,0 +1,29 @@
+const myBio = () => {
+    return {
+        "name": "Deo Gandara Wibawa",
+        "age": 26,
+        "address": "Bandung, Jawa Barat",
+        "hobbies": ["Astronomy", "Watching Movies", "Gaming"],
+        "is_married": true,
+        "list_school": [
+            {
+                "name": "SD Sukasari 2",
+                "year_in": 1992,
+                "year_out": 2005
+
+            }, {
+                "name": "SMPN 1 Baleendah",
+                "year_in": 2005,
+                "year_out": 2008
+            },
+            {
+                "name": "PKBM Darul Fikri",
+                "year_in": 2008,
+                "year_out": 2011
+            }
+        ],
+        "interest_in_coding " : true
+    };
+};
+
+console.log(myBio());
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-WIP
+<p align="center">
+  <img width="300" align="center" src="https://cdn.techinasia.com/data/images/LzvV52o01nNnKw9jBzEU9yTsdLLLNINayjwdwxV3.png" alt="demo"/>
+</p>
+
+# DumbWays Assignment (Deo)
+
+### Soal 1 ([File Jawaban](../../1.js))
+Online demo : https://es6console.com/k0j9trua/
+
+Javascript Object Notation atau disingkat JSON sering dipakai dalam REST API. 
+JSON memiliki karekter sintak yang sangat jelas dan ringan, maka dari itu format JSON sering dipakai untuk bertukar data antara
+Server dan Client begitupun sebaliknya. 
